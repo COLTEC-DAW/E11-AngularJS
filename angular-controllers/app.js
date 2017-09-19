c88eb4e8f2a1d3740f347a38ab36662af3e39aae
Mateuhslc <matheuslcoelho@yahoo.com.br>
2017-09-19 09:37:42 -0300

    Começo do Controller

--- angular-controllers/app.js
+++ angular-controllers/app.js
@@ -13,4 +13,42 @@ app.controller('TemperaturaController', function() {
     }
   };
 
+  this.celsiusToFahrenheit = function(celsius){
+    answer = parseInt(celsius)*1.8+32;
+    if (isNaN(answer)){
+      return 0;
+    } else {
+      return answer;
+    }
+  }
+
+});
+
+app.controller('ImpostoController', function(){
+  this.salario = 0.0;
+
+  this.calculaImposto = function(salario){
+    answer = parseInt(salario);
+
+    if(salario < 2000){
+      answer = salario * 0.1;
+    } else if(salario > 3500){
+      answer = salario * 0.3;
+    } else {
+      answer = salario * 0.2;
+    }
+    if(isNaN(answer)){
+      return 0;
+    } else {
+      return answer;
+    }
+  }
+});
+
+app.controller('reviewController', function(){
+  
+});
+
+app.controller('ContactController', function(){
+
 });
