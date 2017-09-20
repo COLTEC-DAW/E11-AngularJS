1179306fc9de62c9fea55fac295a42b78391fe36
Gustavo de Paula <gdpaula2000@gmail.com>
2017-09-20 14:56:28 -0300

    exercícios controllers

--- angular-controllers/app.js
+++ angular-controllers/app.js
@@ -13,4 +13,101 @@ app.controller('TemperaturaController', function() {
     }
   };
 
+  this.celsiusToFahrenheit = (celsius) => {
+    answer = parseInt(celsius) * (9/5) + 32
+    if (isNaN(answer)) {
+      return 0
+    } else {
+      return answer
+    }
+  }
+
 });
+
+app.controller('RouboController', function() {
+
+  this.salario = 1000
+
+  this.calculateTheft = (salario) => {
+    if (isNaN(salario)) {
+      return 0
+    } else if (salario < 2000) {
+      return salario/10
+    } else if (salario < 3500) {
+      return salario/10 * 2
+    } else {
+      return salario/10 * 3 
+    }
+  }
+
+})
+
+app.controller('ReviewController', function() {
+
+  this.reviews = []
+  this.review = {
+    stars: 0,
+    comment: ""
+  }
+
+  this.addReview = (stars, comment) => {
+    let actualReview = {
+      stars,
+      comment
+    }
+    this.reviews.push(actualReview)
+    this.review.stars = 0
+    this.review.comment = ""
+  }
+
+})
+
+app.controller('ContactController', function() {
+  this.contacts = []
+  this.contact = {
+    name: "",
+    phone: "",
+    email: ""
+  }
+  this.btnMessage = "Add Contact"
+  this.editIndex = 0
+
+  this.handleBtnClick = (name, phone, email) => {
+    if (this.btnMessage == "Add Contact") {
+      let actualContact = {
+        name,
+        phone,
+        email
+      }
+
+      this.contacts.push(actualContact)
+      this.contact.name = ""
+      this.contact.phone = ""
+      this.contact.email = ""
+    } else {
+      let actualContact = {
+        name,
+        phone,
+        email
+      }
+
+      this.contacts[this.editIndex] = actualContact
+      this.contact.name = ""
+      this.contact.phone = ""
+      this.contact.email = ""
+      this.btnMessage = "Add Contact"
+    }
+  }
+
+  this.removeContact = (index) => {
+    this.contacts.splice(index, 1)
+  }
+
+  this.editContact = (index) => {
+    this.btnMessage = "Update Contact"
+    this.contact.name = this.contacts[index].name
+    this.contact.phone = this.contacts[index].phone
+    this.contact.email = this.contacts[index].email
+    this.editIndex = index
+  }
+})
