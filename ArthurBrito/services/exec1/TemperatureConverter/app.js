32456772b1ea7c23629b6ba5e7f0fe747ff27f5b
arthurbrito99 <arthur.brito1999@outlook.com>
2017-09-26 13:17:30 -0300

    Execs 1 e 2 de services.

---
+++ ArthurBrito/services/exec1/TemperatureConverter/app.js
@@ -0,0 +1,42 @@
+var app = angular.module('myApp', []);
+
+app.factory('TemperaturaService', function(){
+
+
+	var TemperaturaService = {};
+
+	this.celsius = 32.0;
+
+	TemperaturaService.celsiusToKelvin = function(celsius) {
+		answer = parseInt(celsius) + 273.5;
+		if (isNaN(answer)) {
+			return 0;
+		} else {
+			return  answer;
+		}
+	};
+
+	TemperaturaService.celsiusToFahrenheit = function(celsius) {
+		answer = parseInt(celsius) * 1.8 + 32;
+		if (isNaN(answer)) {
+			return 0;
+		} else {
+			return  answer;
+		}
+	};
+
+	return TemperaturaService;
+
+})
+
+app.controller('TemperaturaController', ['TemperaturaService', function(TemperaturaService) {
+
+	this.Fahrenheit = function(celsius){
+		return TemperaturaService.celsiusToFahrenheit(celsius);
+	}
+
+	this.Kelvin = function(celsius){
+		return TemperaturaService.celsiusToKelvin(celsius);
+	}
+
+}]);
