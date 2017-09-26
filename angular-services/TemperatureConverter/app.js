f176f31722fe32a5ba588db7a42613caea0ff1ed
VictorHSMoura <victorhugosmoura@gmail.com>
2017-09-26 11:34:01 -0300

    Começo de services

--- angular-services/TemperatureConverter/app.js
+++ angular-services/TemperatureConverter/app.js
@@ -1,10 +1,11 @@
 var app = angular.module('myApp', []);
 
-app.controller('TemperaturaController', function() {
+app.factory('ConversorTemperatura', function(){
+  var tempService = {};
 
-  this.celsius = 32.0;
+  tempService.temps = ['K', 'C', 'F'];
 
-  this.celsiusToKelvin = function(celsius) {
+  tempService.celsiusToKelvin = function(celsius){
     answer = parseInt(celsius) + 273.5;
     if (isNaN(answer)) {
       return 0;
@@ -13,7 +14,7 @@ app.controller('TemperaturaController', function() {
     }
   };
 
-  this.celsiusToFahrenheit = function(celsius) {
+  tempService.celsiusToFahrenheit = function(celsius) {
     answer = parseInt(celsius) * 1.8 + 32;
     if (isNaN(answer)) {
       return 0;
@@ -21,5 +22,17 @@ app.controller('TemperaturaController', function() {
       return  answer;
     }
   };
-
+  return tempService;
 });
+
+app.controller('TemperaturaController', ['ConversorTemperatura', function(conversor) {
+
+  this.celsius = 32.0;
+  this.conversor = conversor;
+  this.kelvin = function(celsius){
+    return conversor.celsiusToKelvin(celsius);
+  }
+  this.fahrenheit = function(celsius){
+    return conversor.celsiusToFahrenheit(celsius);
+  }
+}]);
