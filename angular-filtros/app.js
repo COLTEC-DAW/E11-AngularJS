5fa6cf67dc7bf0f0a1f1eb6e09248b0ef3987482
VictorHSMoura <victorhugosmoura@gmail.com>
2017-09-28 11:44:18 -0300

    Filtros terminados!

--- angular-filtros/app.js
+++ angular-filtros/app.js
@@ -5,67 +5,93 @@ app.controller('FuncionariosController', function() {
   this.employees = [
     {
       name: "João",
-      cpf: 11122233344,
+      cpf: "11122233344",
       email: "joao@j.com",
       phone: 3132453499,
       salary: 100000
     },
     {
       name: "Pedro",
-      cpf: 12312345699,
+      cpf: "12312345699",
       email: "pedro@p.com",
       phone: 2731458976,
       salary: 8600
     },
     {
       name: "Paula",
-      cpf: 12300012311,
+      cpf: "12300012311",
       email: "paula@p.com",
       phone: 4134568900,
       salary: 4550
     },
     {
       name: "Ana",
-      cpf: 08007611134,
+      cpf: "08007611134",
       email: "ana@a.com",
       phone: 2125366556,
       salary: 5500
     },
     {
       name: "Gustavo",
-      cpf: 22244466688,
+      cpf: "22244466688",
       email: "Gustavo Augusto",
       phone: 1125268441,
       salary: 15000
     },
     {
       name: "Marques",
-      cpf: 45465689812,
+      cpf: "45465689812",
       email: "marques@m.com",
       phone: 8899009900,
       salary: 56000
     },
     {
       name: "Matheus",
-      cpf: 07689155189,
+      cpf: "07689155189",
       email: "matheus@m.com",
       phone: 5789550046,
       salary: 3700
     },
     {
       name: "Clara",
-      cpf: 06587921125,
+      cpf: "06587921125",
       email: "mariaclara@maria.com",
       phone: 3125168998,
       salary: 5800
     },
     {
       name: "Claudio",
-      cpf: 45378699900,
+      cpf: "45378699900",
       email: "claudio@claudio.com",
       phone: 1225651398,
       salary: 1500
     }
   ];
 
+});
+
+app.filter('cpfFormat', function(){
+  return function(cpf){
+    var newCpf = "";
+    for(var i = 0; i < cpf.length; i++){
+      if(i == 3 || i == 6) newCpf += '.';
+      else if(i == 9) newCpf += '-';
+      newCpf += cpf.charAt(i);
+    }
+    return newCpf;
+  }
+});
+
+app.filter('telFormat', function(){
+  return function(tel){
+    var telStr = tel.toString();
+    var newTel = "";
+    for(var i = 0; i < telStr.length; i++){
+      if(i == 0) newTel += '(';
+      else if(i == 2) newTel += ')';
+      else if(i == 6) newTel += '-';
+      newTel += telStr.charAt(i);
+    }
+    return newTel;
+  }
 });
