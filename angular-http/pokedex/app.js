4585f0cc0c6173bf6a9c29974f3a774bbe0416fb
franciscofabneves <franciscofabneves@gmail.com>
2017-10-09 09:19:53 -0300

    .

--- angular-http/pokedex/app.js
+++ angular-http/pokedex/app.js
@@ -1,7 +1,4 @@
-
 var app = angular.module('pokedex', []);
-
-
 
 app.factory('PokedexService', function($http){
 
@@ -20,11 +17,16 @@ app.factory('PokedexService', function($http){
       callback(answer);
     });
   };
-
-  pokedexService.getPokemonDescriptionById = function(id, callback) {
-    // TODO: implementar código para recuperar os detalhes de um pokemon por seu id
+  pokedexService.getOnePoke = function(num, callback){
+    $http.get("https://pokeapi.com/api/v1/pokemon/"+num+"/").then(function(response){
+      var answer = response.data;
+      callback(answer);
+    },
+    function(response){
+      var answer = null;
+      callback(answer);
+    });
   };
-
   return pokedexService;
 });
 
@@ -38,5 +40,14 @@ app.controller('PokedexController', ['PokedexService', function(pokedexService){
       self.pokemons = answer;
     }
   });
+  this.expandPoke = function(num){
+    console.log("scouting "+num);
+    pokedexService.getOnePoke(num, function(answer){
+      this.pokemon[num].type1 = answer.types[0].name;
+      this.pokemon[num].type2 = answer.types[1].name;
+      console.log("busca feita");
+      console.log(this.pokemon[num]);
+    })
+  }
 
 }]);
