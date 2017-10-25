02f5685bd327f35c619b00a4a999b92a45db067a
wendervitor <vitornogueira502@gmail.com>
2017-10-25 18:02:53 -0200

    data-binding

--- angular-controllers/app.js
+++ angular-controllers/app.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', []);
 
 app.controller('TemperaturaController', function() {
 
-  this.celsius = 32.0;
+  this.celsius = 38.0;
 
   this.celsiusToKelvin = function(celsius) {
     answer = parseInt(celsius) + 273.5;
@@ -13,4 +13,49 @@ app.controller('TemperaturaController', function() {
     }
   };
 
+  this.celsiusToFht = function(celsius){
+  	answer = (parseInt(celsius) * 1.8) + 32;
+  	if (isNaN(answer)) {
+      return 0;
+    } else {
+      return  answer;
+    }
+  };
+
 });
+
+app.controller('impostoController',function(){
+	this.salario = 0;
+
+	this.calculaimposto = function(salario){
+		if(salario < 2000){
+			answer = salario*0.10;
+		}
+		else if(salario > 3500){
+			answer = salario * 0.30;
+		}
+		else{
+			answer = salario * 0.20;
+		}
+		if (isNaN(answer)) {
+     		return 0;
+    	} else {
+      		return  answer;
+    	}
+	};
+
+});
+
+app.controller('reviewController',function($scope){
+	$scope.comment;
+	$scope.nota;
+	$scope.reviews = [];
+	$scope.criaReview = function(){
+		var review = {
+			comment: $scope.comment,
+			nota: $scope.nota
+		};
+		$scope.reviews.push(review);
+		//console.log(review);
+	}
+})
