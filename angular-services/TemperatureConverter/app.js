6e5784e17e471115a5f6abe59d72a26a6945e2a9
wendervitor <vitornogueira502@gmail.com>
2017-10-31 12:03:40 -0200

    services, quase tudo

--- angular-services/TemperatureConverter/app.js
+++ angular-services/TemperatureConverter/app.js
@@ -1,25 +1,33 @@
 var app = angular.module('myApp', []);
 
-app.controller('TemperaturaController', function() {
+app.service('temperaturaService', function(){
+    this.celsius = 32.0;
+    
+    this.celsiusToKelvin = function(celsius) {
+      answer = parseInt(celsius) + 273.5;
+      if (isNaN(answer)) {
+        return 0;
+      } else {
+        return  answer;
+      }
+    };
+  
+    this.celsiusToFahrenheit = function(celsius) {
+      answer = parseInt(celsius) * 1.8 + 32;
+      if (isNaN(answer)) {
+        return 0;
+      } else {
+        return  answer;
+      }
+    }; 
+});
+app.controller('TemperaturaController', function(temperaturaService, $scope) {
+  $scope.celsius = 32.0;
+  $scope.celsiusToKelvin = temperaturaService.celsiusToKelvin($scope.celsius);
+  $scope.celsiusToFahrenheit = temperaturaService.celsiusToFahrenheit($scope.celsius);
 
-  this.celsius = 32.0;
-
-  this.celsiusToKelvin = function(celsius) {
-    answer = parseInt(celsius) + 273.5;
-    if (isNaN(answer)) {
-      return 0;
-    } else {
-      return  answer;
-    }
-  };
-
-  this.celsiusToFahrenheit = function(celsius) {
-    answer = parseInt(celsius) * 1.8 + 32;
-    if (isNaN(answer)) {
-      return 0;
-    } else {
-      return  answer;
-    }
-  };
-
+  $scope.calcula = function(){
+    $scope.celsiusToKelvin = temperaturaService.celsiusToKelvin($scope.celsius);
+    $scope.celsiusToFahrenheit = temperaturaService.celsiusToFahrenheit($scope.celsius);
+  }
 });
