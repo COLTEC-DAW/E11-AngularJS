2b09aaf0af73a71bc1288685e6f1bfbb871e8a6f
Fernando Ping <nandinho.lap1@yahoo.com>
2017-10-31 08:40:15 -0200

    opa

--- angular-controllers/app.js
+++ angular-controllers/app.js
@@ -58,4 +58,30 @@ app.controller('reviewController',function($scope){
 		$scope.reviews.push(review);
 		//console.log(review);
 	}
-})
+});
+
+app.controller('contactController',function($scope){
+	$scope.contacts = [];
+	$scope.nome;
+	$scope.telefone;
+	$scope.email;
+
+	$scope.adicionaContatos = function(){
+		var contact = {
+			nome: $scope.nome,
+			telefone: $scope.telefone,
+			email: $scope.email
+		};
+		$scope.contacts.push(contact);
+	}
+
+	$scope.editaContato = function(index){
+		$scope.contacts[index].nome = $scope.nome;
+		$scope.contacts[index].telefone = $scope.telefone;
+		$scope.contacts[index].email = $scope.email;
+	}
+	$scope.removeContato = function(index){
+		//console.log(index);
+		$scope.contacts.splice(index,1);
+	}
+});
