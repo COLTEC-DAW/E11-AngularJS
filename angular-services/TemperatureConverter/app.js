4262aaa5f5fd53f31bc589261c23811822ee5dd2
MariSantos17 <mariana@luarinformatica.com.br>
2018-11-19 08:41:20 -0200

    Services Pronto

--- angular-services/TemperatureConverter/app.js
+++ angular-services/TemperatureConverter/app.js
@@ -1,25 +1,19 @@
 var app = angular.module('myApp', []);
 
-app.controller('TemperaturaController', function() {
-
-  this.celsius = 32.0;
-
-  this.celsiusToKelvin = function(celsius) {
-    answer = parseInt(celsius) + 273.5;
-    if (isNaN(answer)) {
-      return 0;
-    } else {
-      return  answer;
+app.service('CtrlTemp', function() {
+    this.myFunc = function (celsius) {
+        return celsius + 213;
     }
-  };
-
-  this.celsiusToFahrenheit = function(celsius) {
-    answer = parseInt(celsius) * 1.8 + 32;
-    if (isNaN(answer)) {
-      return 0;
-    } else {
-      return  answer;
-    }
-  };
-
 });
+app.controller('myCtrl', function($scope, CtrlTemp) {
+  $scope.kelvin = CtrlTemp.myFunc(1);
+});
+
+app.service('CtrlTemp2', function() {
+    this.myFunc = function (celsius) {
+        return celsius * 1.8 + 32;
+    }
+});
+app.controller('myCtrl', function($scope, CtrlTemp2) {
+  $scope.fahrenheit = CtrlTemp2.myFunc(1);
+}); 
