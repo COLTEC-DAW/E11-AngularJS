5a6689eaf1561915569fe14438eeef9b76104d3d
mateuxs <mateusbzs13@gmail.com>
2018-11-24 23:53:58 -0200

    AngularJS: Controllers *falta a função de edição no 4º item

--- angular-controllers/app.js
+++ angular-controllers/app.js
@@ -13,4 +13,58 @@ app.controller('TemperaturaController', function() {
     }
   };
 
+  this.celsiusToFahrenheit = function(celsius) {
+    answer = parseInt(celsius)*1.8+32;
+    if (isNaN(answer)) {
+      return 0;
+    } else {
+      return  answer;
+    }
+  };
 });
+
+
+app.controller('ImpostoController', function() {
+
+  this.calcImposto = function(salario) {
+    s = parseFloat(salario);
+    if(isNaN(s)){
+      return 0;
+    } else if(s<2000){
+      return s*0.1;
+    } else if(s<=3500){
+      return s*0.2;
+    }else{
+      return s*0.3;
+    }
+  }
+});
+
+
+app.controller('ReviewController', function() {
+  this.reviewsArray=[];
+  this.addComentario = function(review, estrelas){
+    var rev = {
+      "review": review,
+      "estrelas": estrelas
+    }
+    this.reviewsArray.push(rev);
+  }
+});
+
+  app.controller('ContactController', function($scope){
+    this.contArray=[];
+    this.addCont = function(nome, telefone, email){
+      var cont = {
+        "nome": nome,
+        "telefone": telefone,
+        "email": email
+      }
+      this.contArray.push(cont);
+    }
+    this.rmCont = function(contato){
+      index = this.contArray.indexOf(contato);
+      this.contArray.splice(index, 1);
+    }
+   
+  });
