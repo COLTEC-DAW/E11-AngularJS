a816af6cc9c17b93f7a90155e08bdc77c7ad0ab9
mateuxs <mateusbzs13@gmail.com>
2018-11-25 01:53:24 -0200

    AngularJS: Services Ex1

--- angular-services/TemperatureConverter/app.js
+++ angular-services/TemperatureConverter/app.js
@@ -1,25 +1,37 @@
 var app = angular.module('myApp', []);
+app.factory('TemperaturaService', function(){
+  var TemperaturaService = {};
+      
+    TemperaturaService.celsiusToKelvin = function(celsius) {
+      answer = parseInt(celsius) + 273.5;
+      if (isNaN(answer)) {
+        return 0;
+      } else {
+        return  answer;
+      }
+    };
 
-app.controller('TemperaturaController', function() {
+    TemperaturaService.celsiusToFahrenheit = function(celsius) {
+      answer = parseInt(celsius) * 1.8 + 32;
+      if (isNaN(answer)) {
+        return 0;
+      } else {
+        return  answer;
+      }
+    };
 
-  this.celsius = 32.0;
+    return TemperaturaService;
+  }
+);
 
-  this.celsiusToKelvin = function(celsius) {
-    answer = parseInt(celsius) + 273.5;
-    if (isNaN(answer)) {
-      return 0;
-    } else {
-      return  answer;
-    }
-  };
+app.controller('TemperaturaController',['TemperaturaService', function(TemperaturaService) {
 
-  this.celsiusToFahrenheit = function(celsius) {
-    answer = parseInt(celsius) * 1.8 + 32;
-    if (isNaN(answer)) {
-      return 0;
-    } else {
-      return  answer;
-    }
-  };
+  this.celsiusToKelvin = function(celsius){ 
+    return TemperaturaService.celsiusToKelvin(celsius);
+  }
+  
+  this.celsiusToFahrenheit = function(celsius){
+    return TemperaturaService.celsiusToFahrenheit(celsius);
+  }
 
-});
+}]);
